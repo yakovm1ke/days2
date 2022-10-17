77a5e13db7d5a613b1e5588a3d2d29167100d232
Yakovlev Mikhail Alekseevich <yakovmikemikle@gmail.com>
2022-10-17 23:05:13 +0300

    add gh pages

--- src/components/ui/MainWrapper.vue
+++ src/components/ui/MainWrapper.vue
@@ -1,12 +1,18 @@
 <template>
   <main :class="$style.mainWrapper">
-    <slot />
+    <div :class="$style.container">
+      <slot />
+    </div>
   </main>
 </template>
 
 <style module>
 .mainWrapper {
   box-sizing: border-box;
-  padding: var(--size-regular);
+  padding: var(--size-large);
+}
+.container {
+  max-width: 600px;
+  margin: 0 auto;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,18 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: true,
-  },
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+	server: {
+		port: 3000,
+		host: true,
+	},
+	plugins: [vue()],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	base: '/days2/',
+	build: {
+		outDir: './docs',
+	},
 })
